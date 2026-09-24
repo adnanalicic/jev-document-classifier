c2b1857a0aeb172a374796f9e01364183cec2ba7
Adnan Alicic <adnan@leads.ba>
2026-09-24 22:06:05 +0200

    feat: add backend/src/main/java/com/example/jevdocumentclassifier/JevDocumentClassifierApplication.java

---
+++ backend/src/main/java/com/example/jevdocumentclassifier/JevDocumentClassifierApplication.java
@@ -0,0 +1,12 @@
+package com.example.jevdocumentclassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JevDocumentClassifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JevDocumentClassifierApplication.class, args);
+    }
+}
